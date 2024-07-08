31c3351f6542f2f0384115bfd51cbf06d6f5f56a
CTT <cttang99@gmail.com>
2024-07-09 01:05:30 +0800

    4.14 slightly improvement about cmd-line input argument

--- 4.14_main.cpp
+++ 4.14_main.cpp
@@ -2,9 +2,22 @@
 // Create GradeBook object and invoke its determineClassAverage function.
 #include "4.12_GradeBook.h"
 #include <cstdlib>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char **argv) {
+    char ans = 0;
     int max = argc == 2 ? atoi(argv[1]) : 10;
+    do {
+        cout << "set max = " << max << " [y/n]? ";
+        ans = getchar();
+        fflush(stdin);
+    } while (ans != 'y' && ans != 'n');
+    if (ans == 'n') {
+        cout << "USAGE: ./4.14 {int}" << endl;
+        return -1;
+    }
+
     GradeBook myGradeBook("CS101 C++ Programming", max);
 
     myGradeBook.displayMessage();
