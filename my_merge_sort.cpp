7319d7cfe31cc3d461d16de1920b5cf8e2739523
CTT <cttang99@gmail.com>
2024-07-16 18:55:22 +0800

    reduced few lines, make whole program more compact

--- my_merge_sort.cpp
+++ my_merge_sort.cpp
@@ -7,13 +7,13 @@ template <typename aS> void merge_sort(aS &);
 int *merge_sort_sub(int *const, const size_t);
 void print_array(const int *, size_t);
 
-int main(int argc, char **argv) {
-    array<int, 6> arr1{7, 9, 1, 0, 7, 3};         merge_sort(arr1);
-    array<int, 6> arr2{6, 5, 4, 3, 2, 1};         merge_sort(arr2);
-    array<int, 6> arr3{13, -4, 5, 2, 9, 3};       merge_sort(arr3);
-    array<int, 6> arr4{12, 11, 13, 5, 6, 7};      merge_sort(arr4);
-    array<int, 12> arr5{92, 8, 123, 32, 4,
-                        -1, -93, 9, 0, 23, 4, 9}; merge_sort(arr5);
+int main() {
+    array<int, 6> arr1{7, 9, 1, 0, 7, 3};       merge_sort(arr1);
+    array<int, 6> arr2{6, 5, 4, 3, 2, 1};       merge_sort(arr2);
+    array<int, 6> arr3{13, -4, 5, 2, 9, 3};     merge_sort(arr3);
+    array<int, 6> arr4{12, 11, 13, 5, 6, 7};    merge_sort(arr4);
+    array<int, 12> arr5{92, 8, 123, 32, 4, -1,
+                        -93, 9, 0, 23, 4, 9};   merge_sort(arr5);
     return 0;
 }
 
@@ -29,10 +29,8 @@ int *merge_sort_sub(int *const p, const size_t size) {
 
     if (size == 1) return p;
     else {
-        lsize = size / 2;
-        rsize = size - lsize;
-        l = merge_sort_sub(p, lsize);
-        r = merge_sort_sub(p + lsize, rsize);
+        l = merge_sort_sub(p        , lsize = size / 2    );
+        r = merge_sort_sub(p + lsize, rsize = size - lsize);
     }
 
     int *tarr = new int[size]{};
