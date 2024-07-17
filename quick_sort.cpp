618899d2ca0a8855be0b8d8342f9cebd515f6c30
CTT <cttang99@gmail.com>
2024-07-17 20:46:26 +0800

    Quick Short - 90% completed
    i == p, j == left
    呢個sorting algorithm 係最有logic 困難嘅一個，我用咗近一個（~6hrs?），
    終於完成咗9成。
    首先我解決咗（諗清楚）到底i,j index 係點運作：
        假設i 係範圍外減1，j 係範圍內第一個；
        然後根據j 判斷是否<=pivot index 嘅元素值
        係嘅話i++，然後j++（for 下一個loop 判斷）
        如果唔係嘅話，i不變，j++（同上）
        循環直到j到達pivot 嘅位置，一樣嘅判斷
        結束循環
    呢個時條，我卡咗喺點樣終結recusive call function
    又諗咗好一陣間，發現sort 好咗嘅話i 係會到達pivot 嘅位，
    所以我將i == --j 成為recursion 嘅base
    condition，j要遞減一次係因為上面loop 完後，j嘅數係pivot +
    1，所以就減一先得。
    
    然後，我以為全部搞掂，快手打個熱飛機。
    點知，我test 咗好幾set，發現成功率唔係100%，但我又debug
    唔到邊個邏輯位錯咗，所以commit 咗先⋯⋯唉⋯⋯

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,10 +1,12 @@
+// clang-format off
 #include <array>
+#include <iomanip>
 #include <iostream>
 using namespace std;
+int count = 0;
 
 template <typename a> void print_arr(a arr) {
-    for (int val : arr)
-        cout << val << ' ';
+    for (auto val : arr) cout << setprecision(2) << fixed << val << ' ';
     cout.put('\n');
 }
 
@@ -14,44 +16,96 @@ template <typename a, typename t> void arr_swap(a &arr, t &x, t &y) {
     arr[y] = tmp;
 }
 
-template <typename a> void quick_sort(a &arr, int start, int pivot) {
-    bool printed = false;
-    if (pivot == start)
-        return;
-    int i = start - 1, j = start;
-    while (j < pivot) {
-        if (arr[j] <= arr[pivot]) {
-            i++;
-            if (i != j)
-                if (!printed) {
-                    arr_swap(arr, i, j);
-                    printed = true;
-                }
+template <typename a> void quick_sort_sub(a &arr, int left, int pivot) {
+    int p = left - 1;
+    while (left <= pivot) {
+        if (arr[left] <= arr[pivot]) {
+            if (++p != left) arr_swap(arr, p, left);
         }
-        j++;
-        if (!printed) {
-            print_arr(arr);
-            printed = true;
-        }
+        left++;
     }
-    // here got problem
-    if (arr[++i] > arr[pivot]) {
-        arr_swap(arr, i, pivot);
-        quick_sort(arr, 0, i);
-        quick_sort(arr, i + 1, arr.size() - 1);
-    } else {
-        quick_sort(arr, 0, pivot - 1);
-        quick_sort(arr, pivot + 1, arr.size() - 1);
-    }
-
+    if (p == --left) return;
     print_arr(arr);
+    quick_sort_sub(arr, 0, p - 1);
+    quick_sort_sub(arr, p + 1, left);
+}
+
+template <typename a>void quick_sort(a &arr) {
+    cout << ++::count << ".\n";
+    cout << "before: "; print_arr(arr);
+    quick_sort_sub(arr, 0, arr.size() - 1);
+    cout << " after: "; print_arr(arr);
+    cout.put('\n');
 }
 
 int main() {
-    // array<int, 6> arr{7, 9, 2, 3, 1, 8};
-    array<int, 6> arr{1, 9, 5, 3, 6, 3};
-    // array<float, 9> arr{1.1, 9.3, 4.3, 9.1, 7.1, 6.6, 8.4, 4.3, 2.7};
-    quick_sort(arr, 0, arr.size() - 1);
-    // int pivot = arr.size() - 1;
+    array<int, 6> arr1{7, 9, 2, 3, 1, 8};  quick_sort(arr1);
+    array<int, 6> arr2{1, 9, 5, 3, 6, 3};  quick_sort(arr2);
+    array<double, 9> arr3{1.1, 9.3, 4.3,
+           9.1, 7.1, 6.6, 8.4, 4.3, 2.7};  quick_sort(arr3);
+    array<int, 5> arr4{30, 9, 46, 14, 22}; quick_sort(arr4);
+    array<int, 8> arr5{5, 2, 7, 6, 1, 9, 4, 8}; quick_sort(arr5); // <-- only this has issue
+    array<int, 6> arr6{55, 21, 77, 68, 32, 4}; quick_sort(arr6);
+    array<int, 6> arr7{-1, 5, 12, -10, 15, 33}; quick_sort(arr7); // <-- this only got problem
+    array<int, 5> arr8{4, 1, 3, 9, 7}; quick_sort(arr8);
+    array<int, 9> arr9{2, 1, 6, 10, 4, 1, 3, 9, 7}; quick_sort(arr9);
     return 0;
 }
+/* execution result
+* 1.
+* before: 7 9 2 3 1 8 
+* 7 2 3 1 8 9 
+* 1 2 3 7 8 9 
+*  after: 1 2 3 7 8 9 
+* 
+* 2.
+* before: 1 9 5 3 6 3 
+* 1 3 3 9 6 5 
+* 1 3 3 5 6 9 
+*  after: 1 3 3 5 6 9 
+* 
+* 3.
+* before: 1.10 9.30 4.30 9.10 7.10 6.60 8.40 4.30 2.70 
+* 1.10 2.70 4.30 9.10 7.10 6.60 8.40 4.30 9.00 
+* 1.10 2.70 4.30 7.10 6.60 8.40 4.30 9.00 9.00 
+* 1.10 2.70 4.30 4.30 6.60 8.40 7.00 9.00 9.00 
+* 1.10 2.70 4.30 4.30 6.60 7.00 8.00 9.00 9.00 
+*  after: 1.10 2.70 4.30 4.30 6.60 7.00 8.00 9.00 9.00 
+* 
+* 4.
+* before: 30 9 46 14 22 
+* 9 14 22 30 46 
+*  after: 9 14 22 30 46 
+* 
+* 5.
+* before: 5 2 7 6 1 9 4 8 
+* 5 2 7 6 1 4 8 9 
+* 2 1 4 6 5 7 8 9 
+* 1 2 4 6 5 7 8 9 
+*  after: 1 2 4 6 5 7 8 9 
+* 
+* 6.
+* before: 55 21 77 68 32 4 
+* 4 21 77 68 32 55 
+* 4 21 32 55 77 68 
+* 4 21 32 55 68 77 
+*  after: 4 21 32 55 68 77 
+* 
+* 7.
+* before: -1 5 12 -10 15 33 
+*  after: -1 5 12 -10 15 33 
+* 
+* 8.
+* before: 4 1 3 9 7 
+* 4 1 3 7 9 
+* 1 3 4 7 9 
+*  after: 1 3 4 7 9 
+* 
+* 9.
+* before: 2 1 6 10 4 1 3 9 7 
+* 2 1 6 4 1 3 7 9 10 
+* 2 1 1 3 6 4 7 9 10 
+* 1 1 2 3 6 4 7 9 10 
+* 1 1 2 3 4 6 7 9 10 
+*  after: 1 1 2 3 4 6 7 9 10 
+*/
