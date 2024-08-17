b4434f120615826ac7ae50e6b3090b844f2e344d
CTT <cttang99@gmail.com>
2024-08-17 17:26:27 +0800

    難得一次我連啲comment 都打埋，點知係錯L曬，呢本書我保持2分不變。

--- how_to_program_cpp/ch15/15.4.cpp
+++ how_to_program_cpp/ch15/15.4.cpp
@@ -3,15 +3,16 @@
 #include <iostream>
 #include <iterator>
 using namespace std;
-
+// 咪撚睇啲英文comment，本on9 書完全講錯曬每個statement。
 int main() {
     std::cout << "Enter two integers: ";
 
     // create istream_iterator for reading int values from cin
-    std::istream_iterator<int> inputInt{cin};
+    std::istream_iterator<int> inputInt{cin}; // 真正嘅read operation 喺呢度發生
 
     int number1{*inputInt}; // read int from standard input
-    ++inputInt;             // move iterator to next input value
+                            // 呢度只係return 一個啱啱讀取咗嘅值
+    ++inputInt; // move iterator to next input value // 呢度++ 會再讀取另一個值
     int number2{*inputInt}; // read int from standard input
 
     // create ostream_iterator for writing int values to cout
@@ -19,5 +20,6 @@ int main() {
 
     std::cout << "The sum is: ";
     *outputInt = number1 + number2; // output result to cout
+                                    // 就算無dereference 都output 到
     std::cout << endl;
 }
